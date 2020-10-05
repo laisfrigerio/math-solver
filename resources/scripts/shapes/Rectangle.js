69dbe5189ec903d86e3ed8e7cf1b413a0771eef2
Diegoslourenco <diego.lourenco15@gmail.com>
2020-10-04 21:36:13 -0300

    add Rectangle.js and rectangle.test.js

---
+++ resources/scripts/shapes/Rectangle.js
@@ -0,0 +1,13 @@
+class Rectangle {
+    constructor(heigth, width) {
+        this.heigth = heigth;
+        this.width = width;
+    }
+
+    calculateArea() {
+        return this.heigth * this.width;
+    }
+}
+
+export default Rectangle;
+
