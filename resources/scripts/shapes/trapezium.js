361f3e152926369d45490e07464f1e25ab1d2067
Diegoslourenco <diego.lourenco15@gmail.com>
2020-10-16 23:34:02 -0300

    add trapezium.js and test.trapezium.js

---
+++ resources/scripts/shapes/trapezium.js
@@ -0,0 +1,14 @@
+class Trapezium {
+    constructor(baseA, baseB, heigth) {
+        this.baseA = baseA;
+        this.baseB = baseB;
+        this.heigth = heigth;
+    }
+
+    calculateArea() {
+        return ((this.baseA + this.baseB) * this.heigth) / 2;  
+         
+    }
+}
+
+export default Trapezium;
